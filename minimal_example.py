92a8e4be40cb8bc8ee98596cac336263c5b3ccb3
jakeck1 <jkeckadmin@ltgr-doeller17.speedport.ip>
2024-06-10 22:42:31 +0200

    added documentation and fixed some minor issues, restructured code

--- minimal_example.py
+++ minimal_example.py
@@ -11,27 +11,29 @@ normalize = True
 metricMDS = True
 distBeyondNN = True
 
-data, labels = createNonUniformHemisphere(N)
-# data, labels = createSwissRole(N,hole=True,seed=0)
-# data, labels = createFourGaussians(8.2,N)
-# data, labels = createMoons(numberOfPoints,noise=0.1,seed=42)
-# data, labels = createTorus(N,seed=0)
-# data, labels = createMammoth(N,k=30,seed=42)
 
-# data, labels = load_MNIST(N)
-# data, labels = createBreastCancerDataset()
-# data, labels = load_FashionMNIST(N)
+if __name__ == '__main__':
+    data, labels = createNonUniformHemisphere(N)
+    # data, labels = createSwissRole(N,hole=True,seed=0)
+    # data, labels = createFourGaussians(8.2,N)
+    # data, labels = createMoons(numberOfPoints,noise=0.1,seed=42)
+    # data, labels = createTorus(N,seed=0)
+    # data, labels = createMammoth(N,k=30,seed=42)
 
-plot_data(data,labels,title="Initial dataset",display=True)
+    # data, labels = load_MNIST(N)
+    # data, labels = createBreastCancerDataset()
+    # data, labels = load_FashionMNIST(N)
 
-t0=time()
-finalEmbedding, clusterLabels = isumap(data, k, d, 
-    normalize = normalize, distBeyondNN=distBeyondNN, verbose=False, dataIsDistMatrix=False, dataIsGeodesicDistMatrix = False, saveDistMatrix = False, labels=labels, initialization="cMDS", metricMDS=metricMDS, sgd_n_epochs = 1500, sgd_lr=1e-2, sgd_batch_size = None,sgd_max_epochs_no_improvement = 75, sgd_loss = 'MSE', sgd_saveplots_of_initializations=True, sgd_saveloss=True)
-t1 = time()
+    plot_data(data,labels,title="Initial dataset",display=True)
 
-title = "Non-uniform Hemisphere N_" + str(N) + " k_" + str(k) + " beyondNN_" + str(distBeyondNN) + " normalize_" + str(normalize) + " metricMDS_" + str(metricMDS)
-plot_data(finalEmbedding,labels,title=title,display=True)
-print("\nResult saved in './Results/" + title + ".png'")
+    t0=time()
+    finalEmbedding, clusterLabels = isumap(data, k, d,
+        normalize = normalize, distBeyondNN=distBeyondNN, verbose=False, dataIsDistMatrix=False, dataIsGeodesicDistMatrix = False, saveDistMatrix = False, labels=labels, initialization="cMDS", metricMDS=metricMDS, sgd_n_epochs = 1500, sgd_lr=1e-2, sgd_batch_size = None,sgd_max_epochs_no_improvement = 75, sgd_loss = 'MSE', sgd_saveplots_of_initializations=True, sgd_saveloss=True)
+    t1 = time()
 
-printtime("Isumap total time",t1-t0)
+    title = "Non-uniform Hemisphere N_" + str(N) + " k_" + str(k) + " beyondNN_" + str(distBeyondNN) + " normalize_" + str(normalize) + " metricMDS_" + str(metricMDS)
+    plot_data(finalEmbedding,labels,title=title,display=True)
+    print("\nResult saved in './Results/" + title + ".png'")
+
+    printtime("Isumap total time",t1-t0)
 
